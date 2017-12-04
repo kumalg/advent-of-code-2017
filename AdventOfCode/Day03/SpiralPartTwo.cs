8e0f43b1bc939e35dd091a0750f7ea067ace97b6
kumalg <kumalgfilms@gmail.com>
2017-12-04 14:26:10 +0100

    improving solution for 2nd part of 3rd day

--- AdventOfCode/Day03/SpiralPartTwo.cs
+++ AdventOfCode/Day03/SpiralPartTwo.cs
@@ -4,31 +4,24 @@ namespace Day03 {
     internal class SpiralPartTwo {
         public static int ComputeNextNumber(int number) {
             var ring = (int)Math.Ceiling(Math.Sqrt(number));
-            var matrix = new int?[ring, ring];
+            var matrix = new int[ring, ring];
 
-            var centerIndex = ring / 2;
+            var center = ring / 2;
 
-            matrix[centerIndex, centerIndex] = 1;
-            var actualCol = centerIndex;
-            var actualRow = centerIndex;
+            matrix[center, center] = 1;
+            var col = center;
+            var row = center;
 
             for (var i = 1; i <= ring; i++) {
-                int actualNumber;
                 var offset = IsOdd(i) ? 1 : -1;
 
-                for (var j = 0; j < i; j++) {
-                    actualRow += offset;
-                    actualNumber = ComputeValueInCell(matrix, actualRow, actualCol);
-                    matrix[actualRow, actualCol] = actualNumber;
-                    if (actualNumber > number)
-                        return actualNumber;
-                }
-                for (var j = 0; j < i; j++) {
-                    actualCol += offset;
-                    actualNumber = ComputeValueInCell(matrix, actualRow, actualCol);
-                    matrix[actualRow, actualCol] = actualNumber;
-                    if (actualNumber > number)
-                        return actualNumber;
+                for (var j = 0; j < 2; j++) {
+                    for (var k = 0; k < i; k++) {
+                        if (j == 0) row += offset;
+                        else col += offset;
+                        if ((matrix[row, col] = ComputeValueInCell(matrix, row, col)) > number)
+                            return matrix[row, col];
+                    }
                 }
             }
             return 0;
@@ -38,20 +31,12 @@ namespace Day03 {
             return number % 2 != 0;
         }
 
-        private static int ComputeValueInCell(int?[,] matrix, int actualRow, int actualCol) {
+        private static int ComputeValueInCell(int[,] matrix, int row, int col) {
             var result = 0;
-
-            result += matrix[actualRow - 1, actualCol - 1] ?? 0;
-            result += matrix[actualRow - 1, actualCol] ?? 0;
-            result += matrix[actualRow - 1, actualCol + 1] ?? 0;
-
-            result += matrix[actualRow, actualCol - 1] ?? 0;
-            result += matrix[actualRow, actualCol + 1] ?? 0;
-
-            result += matrix[actualRow + 1, actualCol - 1] ?? 0;
-            result += matrix[actualRow + 1, actualCol] ?? 0;
-            result += matrix[actualRow + 1, actualCol + 1] ?? 0;
-
+            for (var i = -1; i < 2; i++) {
+                for (var j = -1; j < 2; j++)
+                    result += matrix[row + i, col + j];
+            }
             return result;
         }
     }
