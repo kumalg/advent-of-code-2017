90ee508615e78939187d4aabebbfca613f662dc1
kumalg <kumalgfilms@gmail.com>
2017-12-08 10:18:35 +0100

    add solution for 8th day

---
+++ AdventOfCode/AdventOfCode/Days/Day08.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Days {
+    public class Day08 {
+        private static void Main() {
+            var instructions = File.ReadAllText("../../Inputs/day08.txt")
+                .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(i => i.Split(' '));
+
+            var result = MaxValueInRegisters(instructions);
+            Console.WriteLine(result.Item1);
+            Console.WriteLine(result.Item2);
+            Console.ReadKey();
+        }
+        
+        private static (int, int) MaxValueInRegisters(IEnumerable<string[]> instructions) {
+            var registers = new Dictionary<string, int>();
+            var maxEver = 0;
+
+            foreach (var instruction in instructions) {
+                if (!registers.ContainsKey(instruction[0]))
+                    registers.Add(instruction[0], 0);
+                if (!registers.ContainsKey(instruction[4]))
+                    registers.Add(instruction[4], 0);
+
+                var condition = false;
+                if (instruction[5] == "<")
+                    condition = registers[instruction[4]] < int.Parse(instruction[6]);
+                else if (instruction[5] == "<=")
+                    condition = registers[instruction[4]] <= int.Parse(instruction[6]);
+                else if (instruction[5] == "==")
+                    condition = registers[instruction[4]] == int.Parse(instruction[6]);
+                else if (instruction[5] == ">=")
+                    condition = registers[instruction[4]] >= int.Parse(instruction[6]);
+                else if (instruction[5] == ">")
+                    condition = registers[instruction[4]] > int.Parse(instruction[6]);
+                else if (instruction[5] == "!=")
+                    condition = registers[instruction[4]] != int.Parse(instruction[6]);
+
+                if (condition)
+                    registers[instruction[0]] += instruction[1] == "inc"
+                        ? int.Parse(instruction[2])
+                        : -int.Parse(instruction[2]);
+
+                if (registers[instruction[0]] > maxEver)
+                    maxEver = registers[instruction[0]];
+            }
+            return (registers.Values.Max(), maxEver);
+        }
+    }
+}
--- AdventOfCode/AdventOfCode/Days/Day20_2016.cs
+++ AdventOfCode/AdventOfCode/Days/Day20_2016.cs
@@ -7,19 +7,21 @@ namespace AdventOfCode.Days {
     class Day20_2016 {
         private static void Main(string[] args) {
             var blacklist = File.ReadAllText("../../Inputs/day20_2016.txt").Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(i => i.Split('-').Select(uint.Parse).ToArray());
-            Console.WriteLine($" Part I: {GetLowestValidIp(blacklist)}");
+            Console.WriteLine($" Part I: {GetLowestValidIp(blacklist, uint.MaxValue)}");
             Console.WriteLine($"Part II: {GetValidCount(blacklist, uint.MaxValue)}");
             Console.ReadKey();
         }
 
-        private static uint GetLowestValidIp(IEnumerable<IEnumerable<uint>> blacklist) {
-            var lowestValidIp = 0u;
-            
-            foreach (var blackRange in blacklist.OrderBy(i => i.Last()))
-                if (lowestValidIp >= blackRange.ElementAt(0))
-                    lowestValidIp = blackRange.ElementAt(1) + 1;
+        private static long GetLowestValidIp(IEnumerable<IEnumerable<uint>> blacklist, long maxValue) {
+            for (long i = 0; i <= maxValue; i++) {
+                var includes = blacklist.Where(nums => i >= nums.ElementAt(0) && i <= nums.ElementAt(1));
+                if (includes.Any())
+                    i = includes.Select(nums => nums.ElementAt(1)).Min();
+                else
+                    return i;
+            }
 
-            return lowestValidIp;
+            return -1;
         }
 
         private static long GetValidCount(IEnumerable<uint[]> blacklist, long maxValue) {
@@ -28,7 +30,7 @@ namespace AdventOfCode.Days {
             for (long i = 0; i <= maxValue; i++) {
                 var includes = blacklist.Where(nums => i >= nums.ElementAt(0) && i <= nums.ElementAt(1));
                 if (includes.Any())
-                    i = includes.OrderBy(nums => nums.ElementAt(1)).First().ElementAt(1);
+                    i = includes.Select(nums => nums.ElementAt(1)).Min();
                 else
                     valid++;
             }
