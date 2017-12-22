85b866e5158715d5ea5acb6d8d55a2a3498c481c
kumalg <kumalgfilms@gmail.com>
2017-12-22 22:54:20 +0100

    add not completed solution for day 22

---
+++ AdventOfCode/AdventOfCode/Days/Day11.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Days {
+    public class Day11 {
+        private static void Main() {
+            var input = File.ReadAllText("../../Inputs/day11.txt").Replace("\n","").Split(',');
+
+            Console.WriteLine($" Part I: {CountDistance(input)}");
+            //Console.WriteLine($"Part II: {}");
+
+            Console.ReadKey();
+        }
+
+        public static int CountDistance(IEnumerable<string> input) {
+            var grouped = input.GroupBy(i => i).Select(i => new { i.Key, count = i.Count() });
+
+            var firstGroup = grouped.Where(i => i.Key == "n" || i.Key == "s").OrderByDescending(i => i.count);
+            var first = new { firstGroup.First().Key, count = firstGroup.First().count - firstGroup.Skip(1).Sum(i => i.count) };
+
+            var secondGroup = grouped.Where(i => i.Key == "ne" || i.Key == "sw").OrderByDescending(i => i.count);
+            var second = new { secondGroup.First().Key, count = secondGroup.First().count - secondGroup.Skip(1).Sum(i => i.count) };
+
+            var thirdGroup = grouped.Where(i => i.Key == "nw" || i.Key == "se").OrderByDescending(i => i.count);
+            var third = new { thirdGroup.First().Key, count = thirdGroup.First().count - thirdGroup.Skip(1).Sum(i=>i.count) };
+
+            var xPos = second.count + third.count + 0.0;
+            var yPos = -first.count + 0.5* second.count - 0.5 * third.count;
+
+            var count = 0;
+
+            while (yPos != 0) {
+                xPos--;
+                yPos += 0.5;
+                count++;
+            }
+
+            return Math.Abs(yPos) < 0.01 ? count + (int)Math.Abs(xPos) : count +(int) Math.Abs(yPos);
+        }
+
+        //public static int CountMaxDistance(IEnumerable<string> input)
+        //{
+            
+        //}
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Days/Day22.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Days {
+    public class Day22 {
+        private static void Main() {
+            var input = File.ReadAllText("../../Inputs/day22_example.txt")
+                .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select((i, indexY) =>
+                    i.ToCharArray()
+                        .Select((c, indexX) => new KeyValuePair<(int, int), bool>((indexX, indexY), c == '#')))
+                .SelectMany(nodes => nodes).ToDictionary(node => node.Key, node => node.Value);
+
+            Console.WriteLine($" Part I: {CountPartOne(input, 10000)}");
+            Console.WriteLine($"Part II: {CountPartTwo(input, 10000000)}");
+            //Console.WriteLine($"Part II: {}");
+
+            Console.ReadKey();
+        }
+
+        public static int CountPartOne(Dictionary<(int, int), bool> input, int steps) {
+            var map = input.ToDictionary(entry => entry.Key, entry => entry.Value);
+            var count = 0;
+
+            var actualX = (int)Math.Sqrt(map.Count) / 2;
+            var actualY = actualX;
+            var direction = Direction.Up;
+
+            for (var step = 0; step < steps; step++) {
+
+                if (!map.ContainsKey((actualX, actualY)))
+                    map.Add((actualX, actualY), false);
+
+                var infected = map[(actualX, actualY)];
+                if (!infected)
+                    count++;
+                map[(actualX, actualY)] = !infected;
+
+                var change = infected
+                    ? TurnRight(actualX, actualY, direction)
+                    : TurnLeft(actualX, actualY, direction);
+
+                actualX = change.Item1;
+                actualY = change.Item2;
+                direction = change.Item3;
+            }
+
+            return count;
+        }
+
+        private static (int, int, Direction) TurnLeft(int x, int y, Direction direction) {
+            var newX = x;
+            var newY = y;
+            switch (direction) {
+                case Direction.Up: {
+                    newX--;
+                    break;
+                }
+                case Direction.Right: {
+                    newY--;
+                    break;
+                }
+                case Direction.Down: {
+                    newX++;
+                    break;
+                }
+                case Direction.Left: {
+                    newY++;
+                    break;
+                }
+            }
+            var newDirection = direction == Direction.Up ? Direction.Left : (direction - 1);
+            return (newX, newY, newDirection);
+        }
+
+        private static (int, int, Direction) TurnRight(int x, int y, Direction direction) {
+            var newX = x;
+            var newY = y;
+            switch (direction) {
+                case Direction.Up: {
+                    newX++;
+                    break;
+                }
+                case Direction.Right: {
+                    newY++;
+                    break;
+                }
+                case Direction.Down: {
+                    newX--;
+                    break;
+                }
+                case Direction.Left: {
+                    newY--;
+                    break;
+                }
+            }
+            var newDirection = direction == Direction.Left ? Direction.Up : (direction + 1);
+            return (newX, newY, newDirection);
+        }
+
+        private static (int, int, Direction) TurnStraight(int x, int y, Direction direction) {
+            var newX = x;
+            var newY = y;
+            switch (direction) {
+                case Direction.Up: {
+                    newY--;
+                    break;
+                }
+                case Direction.Right: {
+                    newX++;
+                    break;
+                }
+                case Direction.Down: {
+                    newY++;
+                    break;
+                }
+                case Direction.Left: {
+                    newX--;
+                    break;
+                }
+            }
+            return (newX, newY, direction);
+        }
+
+        private static (int, int, Direction) TurnBack(int x, int y, Direction direction) {
+            var newX = x;
+            var newY = y;
+            Direction newDirection = direction;
+            switch (direction) {
+                case Direction.Up: {
+                    newY++;
+                    newDirection = Direction.Down;
+                    break;
+                }
+                case Direction.Right: {
+                    newX--;
+                    newDirection = Direction.Left;
+                    break;
+                }
+                case Direction.Down: {
+                    newY--;
+                    newDirection = Direction.Up;
+                    break;
+                }
+                case Direction.Left: {
+                    newX++;
+                    newDirection = Direction.Right;
+                    break;
+                }
+            }
+            return (newX, newY, newDirection);
+        }
+
+        public static int CountPartTwo(Dictionary<(int, int), bool> input, int steps) {
+            var map = input.ToDictionary(entry => entry.Key, entry => entry.Value ? 2 : 0);
+            var count = 0;
+
+            var actualX = (int)Math.Sqrt(map.Count) / 2;
+            var actualY = actualX;
+            var direction = Direction.Up;
+
+            for (var step = 0; step < steps; step++) {
+
+                if (!map.ContainsKey((actualX, actualY)))
+                    map.Add((actualX, actualY), 0);
+
+                var state = map[(actualX, actualY)];
+                if (state == 2)
+                    count++;
+                map[(actualX, actualY)] = (state + 1) % 4;
+
+                (int, int, Direction) change;
+                if (state == 0)
+                    change = TurnLeft(actualX, actualY, direction);
+                else if (state == 1)
+                    change = TurnStraight(actualX, actualY, direction);
+                else if (state == 2)
+                    change = TurnLeft(actualX, actualY, direction);
+                else
+                    change = TurnBack(actualX, actualY, direction);
+
+                actualX = change.Item1;
+                actualY = change.Item2;
+                direction = change.Item3;
+            }
+
+            return count;
+        }
+
+        public enum Direction {
+            Up = 0,
+            Right = 1,
+            Down = 2,
+            Left = 3
+        }
+    }
+}
