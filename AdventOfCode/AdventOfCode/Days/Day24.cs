2caf1ebf0c6e90cd2707f29cfaa4622d7d2b3cdb
kumalg <kumalgfilms@gmail.com>
2017-12-25 03:17:35 +0100

    improving peerformance of day 24

--- AdventOfCode/AdventOfCode/Days/Day24.cs
+++ AdventOfCode/AdventOfCode/Days/Day24.cs
@@ -11,46 +11,50 @@ namespace AdventOfCode.Days {
                 .Select(i => i.Split('/').Select(int.Parse));
 
             var result = Compute(components);
-            Console.WriteLine($" Part I: {result.Item1}");
-            Console.WriteLine($"Part II: {result.Item2}");
+            Console.WriteLine($" Part I: {result.partOne}");
+            Console.WriteLine($"Part II: {result.partTwo}");
             Console.ReadKey();
         }
 
-        private static (int, int) Compute(IEnumerable<IEnumerable<int>> componentsOriginal) {
+        private static (int partOne, int partTwo) Compute(IEnumerable<IEnumerable<int>> componentsOriginal) {
             var components = componentsOriginal.ToList();
-            var permutations = new List<List<IEnumerable<int>>>();
+            var permutations = new List<(int strength, int lastPins, int components)>();
 
             var startComponents = components.Where(i => i.ElementAt(0) == 0);
 
             foreach (var startComponent in startComponents) {
-                var bridge = new List<IEnumerable<int>> { startComponent };
+                var bridge = (startComponent.Sum(), startComponent.Last(), 1);
                 AddBridge(bridge, components.Where(i => !i.SequenceEqual(startComponent)).ToArray(), permutations);
             }
 
-            var strengths = permutations.Select(i => (strength : i.Select(x => x.Sum()).Sum(), length: i.Count));
             return (
-                strengths.OrderByDescending(i => i.strength).First().strength, 
-                strengths.GroupBy(i => i.length).OrderByDescending(i => i.Key).First().OrderByDescending(i => i.strength).First().strength);
-
+                permutations.OrderByDescending(i => i.strength).First().strength,
+                permutations.OrderByDescending(i => i.components).ThenByDescending(i => i.strength).First().strength);
         }
 
-        private static void AddBridge(List<IEnumerable<int>> bridge, IEnumerable<IEnumerable<int>> components, ICollection<List<IEnumerable<int>>> permutations) {
+        private static void AddBridge(
+            (int strength, int lastPins, int components) bridge, 
+            IEnumerable<IEnumerable<int>> components, 
+            ICollection<(int strength, int lastPins, int components)> permutations) {
+
             permutations.Add(bridge);
 
             if (!components.Any())
                 return;
-
-            var lastComponent = bridge.Last();
-            var matchedComponents = components.Where(i => i.Contains(lastComponent.ElementAt(1)));
+            
+            var matchedComponents = components.Where(i => i.Contains(bridge.lastPins));
 
             if (!matchedComponents.Any())
                 return;
 
             foreach (var matchedComponent in matchedComponents) {
-                var newBridge = bridge.ToList();
-                newBridge.Add(newBridge.Last().ElementAt(1) != matchedComponent.ElementAt(0)
-                    ? matchedComponent.Reverse()
-                    : matchedComponent);
+                var newBridge = bridge;
+                newBridge.strength += matchedComponent.Sum();
+                newBridge.components++;
+                newBridge.lastPins = newBridge.lastPins != matchedComponent.ElementAt(0)
+                    ? matchedComponent.Reverse().Last()
+                    : matchedComponent.Last();
+
                 AddBridge(newBridge, components.Where(i => !i.SequenceEqual(matchedComponent)).ToArray(), permutations);
             }
         }
