5bf08d3eaa75c404905fbc0158e365c59c638bd9
kumalg <kumalgfilms@gmail.com>
2017-12-28 16:04:47 +0100

    add solution for day 17

---
+++ AdventOfCode/AdventOfCode/Days/Day17.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AdventOfCode.Days {
+    public class Day17 {
+        public static int Input = 345;
+        private static void Main() {
+            Console.WriteLine($" Part I: {PartOne(Input)}");
+            Console.WriteLine($"Part II: {PartTwo(Input)}");
+            Console.ReadKey();
+        }
+
+        public static (ImmutableList<int> list, int index) GetList(int steps, int lastValue) {
+            return Enumerable
+                .Range(1, lastValue)
+                .Aggregate((list: new[] { 0 }.ToImmutableList(), index: 0),
+                    (a, b) => (a.list.Insert(a.index = (a.index + steps) % a.list.Count + 1, b), a.index));
+        }
+
+        public static int PartOne(int steps) {
+            var list = GetList(steps, 2017);
+            return list.list.ElementAt(list.index + 1);
+        }
+
+        public static int PartTwo(int steps) {
+            var list = GetList(steps, 50_000_000);
+            return list.list.ElementAt(list.list.IndexOf(0) + 1);
+        }
+    }
+}
