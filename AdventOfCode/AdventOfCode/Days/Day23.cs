836948b455963b8ca44d061c78cae372d33f71c6
kumalg <kumalgfilms@gmail.com>
2018-01-02 12:22:18 +0100

    day23 experiments

--- AdventOfCode/AdventOfCode/Days/Day23.cs
+++ AdventOfCode/AdventOfCode/Days/Day23.cs
@@ -5,26 +5,26 @@ using System.Linq;
 
 namespace AdventOfCode.Days {
     public class Day23 {
+        public static readonly IEnumerable<string[]> Instructions = File.ReadAllText("../../Inputs/day23.txt")
+            .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(i => i.Split(' '));
         private static void Main() {
-            var instructions = File.ReadAllText("../../Inputs/day23.txt")
-                .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
-                .Select(i => i.Split(' '));
-
-            var result = PartOne(instructions);
-            Console.WriteLine(result);
+           
+            //Console.WriteLine(PartOne());
+            Console.WriteLine(PartTwo());
             Console.ReadKey();
         }
 
-        private static int PartOne(IEnumerable<string[]> instructions) {
+        private static int PartOne() {
             var registers = new Dictionary<string, int> {
                 { "a", 0}, { "b", 0}, { "c", 0}, { "d", 0}, { "e", 0}, { "f", 0}, { "g", 0}, { "h", 0}
             };
             var actualInstructionIndex = 0;
-            var numberOfInstructions = instructions.Count();
+            var numberOfInstructions = Instructions.Count();
             var muls = 0;
 
             while (actualInstructionIndex < numberOfInstructions) {
-                var actualInstriction = instructions.ElementAt(actualInstructionIndex);
+                var actualInstriction = Instructions.ElementAt(actualInstructionIndex);
                 var type = actualInstriction[0];
                 var register = actualInstriction[1];
                 var value = GetValue(registers, actualInstriction[2]);
@@ -50,15 +50,35 @@ namespace AdventOfCode.Days {
             }
             return muls;
         }
-        private static int PartTwo(IEnumerable<string[]> instructions) {
+        private static int PartTwo() {
             var registers = new Dictionary<string, int> {
                 { "a", 1}, { "b", 0}, { "c", 0}, { "d", 0}, { "e", 0}, { "f", 0}, { "g", 0}, { "h", 0}
             };
             var actualInstructionIndex = 0;
-            var numberOfInstructions = instructions.Count();
+            var numberOfInstructions = Instructions.Count();
+
+            var bylo = false;
+            var bylo2 = false;
 
             while (actualInstructionIndex < numberOfInstructions) {
-                var actualInstriction = instructions.ElementAt(actualInstructionIndex);
+                var actualInstriction = Instructions.ElementAt(actualInstructionIndex);
+
+                if (actualInstructionIndex == 16 && !bylo) {
+                    bylo = true;
+                    registers["e"] = 1;
+                    registers["d"] = 0;
+                    registers["b"] = 1;
+                    actualInstructionIndex++;
+                    continue;
+                }
+
+                //if (actualInstructionIndex == 20 && !bylo2) {
+                //    bylo2 = true;
+                //    registers["d"] = registers["b"];
+                //    actualInstructionIndex++;
+                //    continue;
+                //}
+
                 var type = actualInstriction[0];
                 var register = actualInstriction[1];
                 var value = GetValue(registers, actualInstriction[2]);
@@ -82,7 +102,7 @@ namespace AdventOfCode.Days {
                 actualInstructionIndex++;
 
                 if (register == "h")
-                    Console.WriteLine(registers["h"]);
+                  Console.WriteLine(registers["h"]);
             }
             Console.WriteLine("End");
             return registers["h"];
